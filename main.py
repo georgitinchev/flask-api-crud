9ac1c9ac55407beb653b04d1336144f147bd1d46
Георги Тинчев <georgi.tinchev.124@gmail.com>
2025-04-26 21:13:03 +0300

    final state before database

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
-video_put_args.add_argument("views", type=str, help="Views of the video", required=True)
-video_put_args.add_argument("likes", type=str, help="Likes of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
 
 videos = {}
 
+def abort_if_video_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video id is not valid...")
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video with that id already exists...")
+
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()
-        return {video_id: args}
+        videos[video_id] = args
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
+        del videos[video_id]
+        return "", 204
 
 # can access specified name after string
 api.add_resource(Video, "/video/<int:video_id>")
