d149ea5fb4e98a62f6156b55aa7b26f89b886e7b
Георги Тинчев <georgi.tinchev.124@gmail.com>
2025-04-26 20:29:27 +0300

    arguments parser vol 1

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-from flask import flask
-from flask_restful import Api, Resource
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=str, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=str, help="Likes of the video", required=True)
+
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
+
+# can access specified name after string
+api.add_resource(Video, "/video/<int:video_id>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
